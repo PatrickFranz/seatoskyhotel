2b38a4cee85c6fd724a8b2dfab1c636ddeb23e05
PatrickFranz <clymbing@gmail.com>
2018-10-31 11:21:58 -0700

    Add Google Translate element Removed my Language elements Fixes to RWD Fixes to layout

--- dist/js/handlebars_template.js
+++ dist/js/handlebars_template.js
@@ -3,14 +3,18 @@
 const hbsHeaderContainer = document.querySelector('.hbs-header');
 const hbsContentContainer = document.querySelector('.hbs-main-content');
 const hbsFooterContainer = document.querySelector('.hbs-footer');
+const hbsModalContainer = document.querySelector('.hbs-modal');
 
 const header_src = document.querySelector("#hbs_header").innerHTML;
 const body_src = document.querySelector("#hbs_body").innerHTML;
 const footer_src = document.querySelector("#hbs_footer").innerHTML;
+const modal_src = document.querySelector("#hbs_modal").innerHTML;
 
 const header_template = Handlebars.compile(header_src);
 const body_template = Handlebars.compile(body_src);
 const footer_template = Handlebars.compile(footer_src);
+const modal_template = Handlebars.compile(modal_src);
+
 
 const json = fetch('js/siteData.json')
   .then( resp => resp.json())
@@ -21,7 +25,6 @@ const json = fetch('js/siteData.json')
       insertAdjacentHTML('beforeend',body_template(json));
     hbsFooterContainer.
       insertAdjacentHTML('beforeend',footer_template(json));
-
   }).then( () => init() ) 
 
 
@@ -33,5 +36,10 @@ Handlebars.registerHelper('linkReplace', (originalText, url, textToReplace, repl
 Handlebars.registerHelper('getDate', (offset=0) => {
   const d = new Date();
   return new Handlebars.SafeString(`${d.getDate()}`);
+});
+
+Handlebars.registerHelper('showModal', (options) => {
+  console.log(options.fn());
+  return options.fn(this)
 });
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,60 @@
 
+"use strict";
 function init(){
-  const btnReservation = document.querySelector('.book-btn');
+  const btnsMenu = document.querySelectorAll('.menu-btn');
+  const btnsReservation = document.querySelectorAll('.book-btn');
   const resBarElement = document.querySelector('.res-bar'); 
-  const headerBtns = Array.from(document.querySelectorAll('.header-right > button'));
-  btnReservation.addEventListener('click', toggleResBar);
+  
 
+  const menuBtns = Array.from(document.querySelectorAll('.header-right > button'));
+
+  btnsMenu.forEach( e => {
+    e.addEventListener('click', e => {
+      toggleModal(e, {"context": "nav"});
+    });
+  });
+
+  btnsReservation.forEach( e => {
+    e.addEventListener('click', toggleResBar);
+  });
+  
   function toggleResBar(e){
-    console.log('toggle');
     resBarElement.classList.toggle('show');
   }
+
+
+  function toggleModal(e, contextName){
+    fetch('js/siteData.json')
+      .then(resp => resp.json())
+      .then(data => {
+        hbsModalContainer
+        .insertAdjacentHTML('beforeend', modal_template(data[contextName.context]));
+
+        let modalElement = document.querySelector('.modal'); 
+        let modalCloseButton = modalElement.querySelector('.modal--close');
+        let modalContentLinks = modalElement.querySelectorAll('.modal--list-item');
+        modalContentLinks[0].classList.add('selected');
+        modalContentLinks.forEach( (link) => {
+          link.addEventListener('click', (e) => {
+            modalContentLinks.forEach(el => el.classList.remove('selected'));
+            e.currentTarget.classList.add('selected');            
+          })
+        });
+
+        setTimeout( () => {
+          modalElement.style.opacity = 1;
+        }, 0);
+        modalElement.style.display = 'grid';
+
+        modalCloseButton.addEventListener('click', e => {
+          setTimeout( () => {
+            modalElement.style.display = 'none';
+            hbsModalContainer.innerHTML = "";
+          }, 500);
+          modalElement.style.opacity = 0;
+        });
+      });
+  }
+
+
 }
